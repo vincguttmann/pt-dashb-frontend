805545b5588b6a8cb84397b7d0fa0a6ec5529a5e
fischerliste <fischerliste@gmail.com>
2022-11-29 15:39:55 +0100

    layout building

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'dart:convert';
+import 'package:http/http.dart';
 
 import 'DataObjects.dart';
 import 'package:flutter/material.dart';
@@ -7,20 +7,22 @@ import 'package:http/http.dart' as http;
 
 Stream<RootData> fetchData() async* {
   while (true) {
-    await Future.delayed(Duration(seconds: 30));
     String githubURL =
         'https://raw.githubusercontent.com/vincguttmann/pt-dashb-frontend/master/assets/station.json';
-    final response = await http.get(Uri.parse(githubURL));
+    print('getting URL now');
+    Response response = await http.get(Uri.parse(githubURL));
 
     if (response.statusCode == 200) {
+      print('response 200!');
       // If the server did return a 200 OK response,
       // then parse the JSON.
-      yield RootData.fromString(jsonDecode(response.body));
+      yield RootData.fromString(response.body);
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
       throw Exception('Failed to load data');
     }
+    await Future.delayed(const Duration(seconds: 45));
   }
 }
 
@@ -34,7 +36,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     super.initState();
@@ -48,21 +49,154 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.indigo,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Fetch Data Example'),
+        body: StreamBuilder<RootData>(
+          stream: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              print('new build triggered!');
+              print(snapshot.data!.stations[0].departures[0].destination);
+              return buildLayout(context, snapshot.data!);
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            return const CircularProgressIndicator();
+          },
         ),
-        body: Center(
-          child: StreamBuilder<RootData>(
-            stream: fetchData(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.toString());
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              return const CircularProgressIndicator();
-            },
+      ),
+    );
+  }
+}
+
+Widget buildLayout(BuildContext context, RootData rootdata) {
+  if (rootdata.stations.length == 7) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Expanded(
+          child: IntrinsicHeight(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(child: StationWidget(station: rootdata.stations[0])),
+                const VerticalDivider(
+                  indent: 0,
+                  endIndent: 0,
+                  width: 2,
+                  thickness: 2,
+                  color: Colors.grey,
+                ),
+                Flexible(child: StationWidget(station: rootdata.stations[1])),
+                const VerticalDivider(
+                  indent: 0,
+                  endIndent: 0,
+                  width: 2,
+                  thickness: 2,
+                  color: Colors.grey,
+                ),
+                Flexible(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      StationWidget(station: rootdata.stations[2]),
+                      const Divider(
+                        endIndent: 0,
+                        indent: 0,
+                        color: Colors.grey,
+                        thickness: 2,
+                        height: 2,
+                      ),
+                      StationWidget(station: rootdata.stations[3]),
+                      Container(),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
+        ),
+        const Divider(
+          endIndent: 0,
+          indent: 0,
+          color: Colors.grey,
+          thickness: 2,
+          height: 2,
+        ),
+        IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(child: StationWidget(station: rootdata.stations[4])),
+              const VerticalDivider(
+                indent: 0,
+                endIndent: 0,
+                width: 2,
+                thickness: 2,
+                color: Colors.grey,
+              ),
+              Flexible(child: StationWidget(station: rootdata.stations[5])),
+              const VerticalDivider(
+                indent: 0,
+                endIndent: 0,
+                width: 2,
+                thickness: 2,
+                color: Colors.grey,
+              ),
+              Flexible(child: StationWidget(station: rootdata.stations[6])),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+  return Text('Ein Fehler ist aufgetreten!');
+}
+
+class StationWidget extends StatelessWidget {
+  const StationWidget({super.key, required this.station});
+
+  final Station station;
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: <DataColumn>[
+        const DataColumn(
+          label: Center(
+            child: Text(''
+            ),
+          ),
+        ),        DataColumn(
+          label: Center(
+            child: Text(
+              station.name,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+        ),        DataColumn(
+          label: Center(
+            child: Text(
+              'min',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+        ),
+      ],
+      rows: List<DataRow>.generate(
+        station.departures.length,
+        (int index) => DataRow(
+          /// color: MaterialStateProperty.resolveWith<Color?>(
+          ///         (Set<MaterialState> states) {
+          ///       // All rows will have the same selected color.
+          ///       if (states.contains(MaterialState.selected)) {
+          ///         return Theme.of(context).colorScheme.primary.withOpacity(0.08);
+          ///       }
+          ///       // Even rows will have a grey color.
+          ///       if (index.isEven) {
+          ///         return Colors.grey.withOpacity(0.3);
+          ///       }
+          ///       return null; // Use default value for other states and odd rows.
+          ///     }),
+          cells: <DataCell>[DataCell(Text(station.departures[index].label ?? "")),DataCell(Text(station.departures[index].destination)),DataCell(Text(station.departures[index].times.toString()))],
         ),
       ),
     );
